2d70cbb845ee3dee5f484e94294fcdd7aa9fb22d
Marco Villarruel <marco@wildberry.us>
2023-12-28 18:53:34 +0200

    Corrected jenkins script that apply terrafom changes to utilize our AWS enviroment variables.

--- jenkins-packer-demo/scripts/jenkins-run-terraform.sh
+++ jenkins-packer-demo/scripts/jenkins-run-terraform.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ex
-AWS_REGION="eu-west-1"
-cd jenkins-packer-demo
-S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -d ' ' -f3`
+AWS_REGION="us-east-1"
+cd jenkins-packer-demo || exit
+S3_BUCKET=$(aws s3 ls --region $AWS_REGION | awk '{for (i=1; i<=NF; i++) if ($i ~ /^terraform-state/) print $i}' | head -n1)
 sed -i 's/terraform-state-xx70dpnh/'${S3_BUCKET}'/' backend.tf
 sed -i 's/#//g' backend.tf
 aws s3 cp s3://${S3_BUCKET}/amivar.tf amivar.tf --region $AWS_REGION
